4e015391705ed51782a6b77a17822af0ee8417ca
Keila Roberta Da Silva <137004868+keilarobertasv@users.noreply.github.com>
2023-07-26 23:27:29 -0300

    Classe de escolha e método executar

--- src/Capitulo.java
+++ src/Capitulo.java
@@ -1,66 +1,79 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Capitulo {
     private String nome;
     private String texto;
-    private String [] escolhas;
+    private ArrayList<Escolha> escolhas;
     private Personagem personagem;
     private Scanner scanner;
 
-    public Capitulo(String nome, String texto, String[] escolhas, Personagem personagem, Scanner scanner) {
+    public Capitulo(String nome, String texto, Personagem personagem, Scanner scanner) {
         this.nome = nome;
         this.texto = texto;
-        this.escolhas = escolhas;
+        this.escolhas = new ArrayList<>();
         this.personagem = personagem;
         this.scanner = scanner;
     }
+   public void executar() {
+    mostrar();
+    Capitulo proximoCapitulo = escolher();
+    proximoCapitulo.executar();
+}
 
     public void mostrar() {
-        System.out.println("-------------------");
+
         System.out.println(nome);
         System.out.println(texto);
-        if (escolhas[0] != null){
-            System.out.println(escolhas[0]);
-        }
-        if (escolhas[1] != null){
-            System.out.println(escolhas[1]);
-        }
-        
-        
         System.out.println("-------------------");
-        
-        if (this.personagem.vivo){
-          personagem.cansar();
+         if (this.personagem.vivo) {
+            personagem.cansar();
+        } else {
+            this.personagem.morrer();
         }
-        else{this.personagem.morrer();}
-        
-
-        
-        
+        System.out.println("-------------------");
     }
 
-    public int escolher() {
+    public void addEscolha(Escolha escolha) {
+        escolhas.add(escolha);
+    }
+
+    public Capitulo escolher() {
         int resposta = 0;
         boolean escolhaValida = false;
-
+    
         do {
+            for (int i = 0; i < escolhas.size(); i++) {
+                if (escolhas.get(i) == null){
+                    System.out.println("FIM");
+                }
+            }
+
+            for (int i = 0; i < escolhas.size(); i++) {
+                System.out.println(escolhas.get(i).getTexto());
+            }
+    
             System.out.print("Digite sua escolha: ");
             String input = scanner.nextLine();
-
-            if (input.equalsIgnoreCase(escolhas[0])) {
-                resposta = 1;
-                escolhaValida = true;
-            } else if (input.equalsIgnoreCase(escolhas[1])) {
-                resposta = 2;
-                escolhaValida = true;
-            } else {
-                System.out.println("Escolha inválida. Digite uma escolha válida.");
+    
+            for (int i = 0; i < escolhas.size(); i++) {
+                if (escolhas.get(i).getTexto().equalsIgnoreCase(input)) {
+                    resposta = i + 1;
+                    escolhaValida = true;
+                    break;
+                }
+            }
+    
+            if (!escolhaValida) {
+                System.out.println("Escolha inválida. Digite uma opção válida.");
             }
         } while (!escolhaValida);
-
-        return resposta;
+        return escolhas.get(resposta - 1).getProximo();
+        
     }
-
     
-    
+        
 }
+    
+
+
